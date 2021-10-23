d27b9542acee6ffbf95c2cdbb3fc2c372b3ac66c
Kesavaraj80 <mkesavaraj1998@gmail.com>
2021-10-24 02:04:54 +0530

    Button added

--- src/MovieDetails.js
+++ src/MovieDetails.js
@@ -1,21 +1,24 @@
 import React from "react";
-import { useParams } from "react-router-dom";
-import {useState, useEffect} from "react";
+import { useParams, useHistory } from "react-router-dom";
+import { useState, useEffect } from "react";
+import Button from '@mui/material/Button';
 import './App.css';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 
 export function MovieDetails() {
+  const history = useHistory();
   const { id } = useParams();
   const [movie, setMovie] = useState({});
 
-  const getMovie =() => {
-    fetch("https://6173de34110a740017223187.mockapi.io/movies/"+id)
+  const getMovie = () => {
+    fetch("https://6173de34110a740017223187.mockapi.io/movies/" + id)
       .then(data => data.json())
       .then(mve => setMovie(mve));
   };
   useEffect(getMovie);
   console.log(movie);
   return (
-    <div>
+    <div className="Movie_Details_container">
       <iframe
         width="100%"
         height="544"
@@ -25,6 +28,11 @@ export function MovieDetails() {
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
         allowfullscreen>
       </iframe>
+      <div className="back_button">
+        <Button variant="contained" size="large"  startIcon={<ArrowBackIcon/>}onClick={() => {
+          history.push("/movies");
+        }}>Back</Button>
+      </div>
     </div>
   );
 
